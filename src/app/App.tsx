597120368e9afa9ced6ce02fa724f513265bf924
Vivadh Khiev <khievvivadh@gmail.com>
2024-07-22 17:03:30 +0700

    set up folder structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../_xelth-info/assets/style/App.css';
 
 function App() {
   return (
